723764af191218b354585a05ee6a95dcc8726044
Joe Pea <joe@trusktr.io>
2023-08-24 18:55:25 -0700

    show a reproduction for Meteor issue 12759

--- public/AutoApp.js
+++ public/AutoApp.js
@@ -1,10 +1,24 @@
 // @ts-check
+import '/make-three-global.js'
 import { Meteor } from 'meteor/meteor'
 import { defineElements } from 'lume'
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.127.0/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.127.0/examples/jsm/loaders/GLTFLoader.js'
-import { VRButton } from 'https://cdn.jsdelivr.net/npm/three@0.127.0/examples/jsm/webxr/VRButton.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { VRButton } from 'three/examples/jsm/webxr/VRButton.js'
 //   import * as HOST from "../src/three.js/index.js";
+
+/** @param {string} url */
+async function loadNonModuleScript(url) {
+	const script = document.createElement('script')
+
+	const code = await fetch(url).then(r => r.text())
+	script.textContent = code
+
+	console.log(code)
+	document.body.append(script)
+}
+
+await loadNonModuleScript('/host.three.js')
 
 defineElements()
 
@@ -576,7 +590,7 @@ class AutoApp extends HTMLElement {
 		})
 		groundMat.metalness = 0
 		groundMat.refractionRatio = 0
-		const ground = new THREE.Mesh(new THREE.PlaneBufferGeometry(100, 100), groundMat)
+		const ground = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), groundMat)
 		ground.rotation.x = -Math.PI / 2
 		ground.receiveShadow = true
 		scene.add(ground)
